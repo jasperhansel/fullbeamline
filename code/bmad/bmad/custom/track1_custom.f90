d897d979dba4a8d50c49b7a8259709d1292dfa0d
Jasper Hansel <jasperhansel@gmail.com>
2017-11-08 15:26:28 -0800

    multiparticle tracking works with gpt

--- code/bmad/bmad/custom/track1_custom.f90
+++ code/bmad/bmad/custom/track1_custom.f90
@@ -47,81 +47,8 @@ type (track_struct), optional :: track
 
 logical err_flag, finished
 
-character(*), parameter :: r_name = 'track1_custom'
-
-integer :: fileno, error_indicator
-
-real(rp) :: x, y, bx, by, bz, G, q
-
-! general
-finished = .true.
-
-! generate unique file number
-fileno = lunget()
-
-! create parameters file
-open (fileno, file='__fullbeamline/parameters.txt', status='replace', err=10)
-
-! write custom attributes
-if (trim(attribute_name(ele, custom_attribute1$)) /= '!NULL') &
-  write (fileno, *, err=10) attribute_name(ele, custom_attribute1$), ' = ', ele%value(custom_attribute1$)
-if (trim(attribute_name(ele, custom_attribute2$)) /= '!NULL') &
-  write (fileno, *, err=10) attribute_name(ele, custom_attribute2$), ' = ', ele%value(custom_attribute2$)
-if (trim(attribute_name(ele, custom_attribute3$)) /= '!NULL') &
-  write (fileno, *, err=10) attribute_name(ele, custom_attribute3$), ' = ', ele%value(custom_attribute3$)
-if (trim(attribute_name(ele, custom_attribute4$)) /= '!NULL') &
-  write (fileno, *, err=10) attribute_name(ele, custom_attribute4$), ' = ', ele%value(custom_attribute4$)
-if (trim(attribute_name(ele, custom_attribute5$)) /= '!NULL') &
-  write (fileno, *, err=10) attribute_name(ele, custom_attribute5$), ' = ', ele%value(custom_attribute5$)
-
-! close parameters file
-close (fileno, err=10)
-
-! call gpt
-call system_command('fullbeamline-track-single')
-
-! generate unique file number
-fileno = lunget()
-
-! open result file
-open (fileno, file='__fullbeamline/result.txt', status='old', err=10)
-
-! read error indicator
-read (fileno, *, err=10) error_indicator
-
-! check error indicator
-if (error_indicator /= 0) goto 20
-
-! read data
-read (fileno, *, err=10) x, y, bx, by, bz, G, q
-
-! close file
-close (fileno, err=10, status='delete')
-
-! set particle parameters
 end_orb = start_orb
-end_orb%beta = sqrt(bx ** 2 + by ** 2 + bz ** 2)
-end_orb%vec(1) = x
-end_orb%vec(2) = bx * G * m_electron / end_orb%p0c
-end_orb%vec(3) = y
-end_orb%vec(4) = by * G * m_electron / end_orb%p0c
-end_orb%vec(5) = 0.0
-end_orb%vec(6) = (bz * G * m_electron / end_orb%p0c) - 1
 end_orb%s = ele%s
-end_orb%charge = q
-end_orb%beta = bz
-
-! return
-return
-
-! executed if an io operation fails
-10 call out_io (s_fatal$, r_name, 'IO OPERATION FAILED')
-call err_exit
-return
-
-! executed if tracking failed
-20 end_orb = start_orb
 end_orb%state = lost$
-return
 
 end subroutine
